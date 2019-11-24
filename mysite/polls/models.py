c5502e46858ef1b4270ad0491f335b83582642d0
lmquan1609 <luuminhquan155@gmail.com>
2019-11-24 22:39:54 +0700

    test

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+# After completing db model, auto create db: "python manage.py makemigrations", and then "python manage.py migrate"
+# To test:
+# python manage.py shell
+# from polls.models import Question
+# Question.objects.all()
+# from django.utils import timezone
+# q = Question(question_text='Which color do you like?', time_pub=timezone.now())
+# q.save()
+# q.id
+# q.question_text
+# c = Choice(question=q, choice_text='red', vote=0)
+# c.save()
+# d = Choice(question=q, choice_text='green', vote=0)
+# d.save()
+
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    time_pub = models.DateTimeField()
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    vote = models.IntegerField(default=0)
